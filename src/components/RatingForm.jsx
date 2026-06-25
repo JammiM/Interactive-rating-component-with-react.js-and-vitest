1ded8f1cb5610079ef8393917a521d6fc5165b83
James Morrissey <jammim19@gmail.com>
2026-06-25 14:48:35 +0100

    Outlined the form component

--- src/components/RatingForm.jsx
+++ src/components/RatingForm.jsx
@@ -1,5 +1,26 @@
 import React from "react";
 
 export const RatingForm = () => {
-  return <div>RatingForm</div>;
+  const ratingValues = [1, 2, 3, 4, 5];
+
+  return (
+    <div>
+      RatingForm
+      <form action="" method="post" id="ratingForm">
+        <fieldset>
+          <legend>group of radio's buttons</legend>
+
+          {ratingValues.map((singleRating) => (
+            <label key={singleRating} htmlFor={singleRating}>
+              <input type="radio" name="rating score" id={singleRating} />
+            </label>
+          ))}
+        </fieldset>
+
+        <button type="submit" form="ratingForm">
+          submit
+        </button>
+      </form>
+    </div>
+  );
 };
