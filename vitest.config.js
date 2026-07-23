85c695ce14575586b28bd2369b237542a4805f51
James Morrissey <jammim19@gmail.com>
2026-07-23 11:49:44 +0100

    Updated how testing is configured

--- vitest.config.js
+++ vitest.config.js
@@ -2,9 +2,10 @@ import config from "./vite.config";
 import { defineConfig } from "vitest/config";
 
 export default defineConfig({
+  pllugins: [...config.plugins],
   test: {
     globals: true,
-    //     // environment: "jsdom",
-    //     // setupFiles: "./tests/setup.ts",
+    environment: "jsdom",
+    setupFiles: "./src/tests/vitest-setup.js",
   },
 });
