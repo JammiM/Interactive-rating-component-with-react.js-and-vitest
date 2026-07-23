dc01935b1b956e36d872ae4ea0a19965d11c3a5c
James Morrissey <jammim19@gmail.com>
2026-07-23 12:06:41 +0100

    Added units test for both the App and RatingForm component

---
+++ src/tests/App.test.jsx
@@ -0,0 +1,16 @@
+import { render, screen } from "@testing-library/react";
+
+import App from "../App";
+
+describe("App", () => {
+  it("Renders the correct text content", () => {
+    render(<App />);
+
+    expect(screen.getByText(/How did we do?/i)).toBeInTheDocument();
+    expect(
+      screen.getByText(
+        /Please let us know how we did with your support request. All feedback is appreciated to help us improve our offering!/i,
+      ),
+    ).toBeInTheDocument();
+  });
+});
--- src/tests/RatingForm.test.jsx
+++ src/tests/RatingForm.test.jsx
@@ -1,7 +1,38 @@
 import { describe } from "vitest";
 
+import { render, screen } from "@testing-library/react";
+import { RatingForm } from "../components/RatingForm";
+
 describe("RatingForm", () => {
   it("should render the component", () => {
-    expect(true).toBe(true);
+    render(<RatingForm />);
+
+    expect(screen.getByText(/How did we do?/i)).toBeInTheDocument();
+  });
+
+  it("Renders the Submit button correctly", () => {
+    render(<RatingForm />);
+
+    const submitButtonElement = screen.getByRole("button", { name: /submit/i });
+
+    expect(submitButtonElement).toBeInTheDocument();
+    expect(submitButtonElement).toBeDisabled();
+  });
+
+  it("should enable the submit button when a rating is selected", () => {
+    render(<RatingForm />);
+
+    const radioButton = screen.getByLabelText("3");
+    radioButton.click();
+
+    const submitButtonElement = screen.getByRole("button", { name: /submit/i });
+    expect(submitButtonElement).toBeEnabled();
+  });
+
+  it("should render the rating options correctly", () => {
+    render(<RatingForm />);
+
+    const radioButtons = screen.getAllByRole("radio");
+    expect(radioButtons).toHaveLength(5);
   });
 });
