51d2893675ba6940fc62de2d182343db5dfa4fa1
James Morrissey <jammim19@gmail.com>
2026-08-26 16:44:04 +0100

    Updated the Vitest config to target unit and e2e tests separately

--- vitest.config.js
+++ vitest.config.js
@@ -1,11 +1,32 @@
 import config from "./vite.config";
 import { defineConfig } from "vitest/config";
+import { playwright } from "@vitest/browser-playwright";
 
 export default defineConfig({
   plugins: [...config.plugins],
   test: {
-    globals: true,
-    environment: "jsdom",
-    setupFiles: "./src/tests/vitest-setup.js",
+    projects: [
+      {
+        test: {
+          include: ["./src/tests/unit/**/*.test.{js,jsx}"],
+          name: "unit",
+          globals: true,
+          environment: "jsdom",
+          setupFiles: "./src/tests/vitest-setup.js",
+        },
+      },
+      {
+        test: {
+          include: ["./src/tests/e2e/**/*.browser.test.{js,jsx}"],
+          name: "browser",
+          globals: true,
+          browser: {
+            enabled: true,
+            provider: playwright(),
+            instances: [{ browser: "chromium" }],
+          },
+        },
+      },
+    ],
   },
 });
